07efc6a3bc41d5ea90928d914e24f52dfd6cd7ae
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-09-23 12:02:55 +0530

    Adding Serverside Validation for Email Field

--- subscribe/views.py
+++ subscribe/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def subscribe(request):
-    context = {}
+    email_error = ""
+    if request.POST:
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+
+        if email == "":
+            email_error = "No Email Entered!"
+
+    
+    context = {"email_error": email_error}
     return render(request, "subscribe/subscribe.html", context)
