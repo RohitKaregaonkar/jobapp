f47d32950e4e2850e690e5668a756c85d36a5012
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-09-23 12:15:10 +0530

    Creating New Model Subscribe and Modifying Views.py File to store data inside database by Coventional way.

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from subscribe.models import Subscribe
 
 # Create your views here.
 def subscribe(request):
@@ -11,6 +12,9 @@ def subscribe(request):
         if email == "":
             email_error = "No Email Entered!"
 
-    
+        subscribe = Subscribe(first_name = fname, last_name = lname, email = email)
+        subscribe.save()
+        
+        
     context = {"email_error": email_error}
     return render(request, "subscribe/subscribe.html", context)
