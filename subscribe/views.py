1a30a04edf5bbd7e4f60041f6a982c9858aea6e3
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-08 16:46:50 +0530

    Using cleaned_data() method to access the data and created Subscribe object to Save the form data

--- subscribe/views.py
+++ subscribe/views.py
@@ -10,6 +10,13 @@ def subscribe(request):
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
             print("Valid Form")
+            print(subscribe_form.cleaned_data)
+            fname = subscribe_form.cleaned_data['first_name']
+            lname = subscribe_form.cleaned_data['last_name']
+            email = subscribe_form.cleaned_data['email']
+            
+            subscribe = Subscribe(first_name= fname, last_name= lname, email= email)
+            subscribe.save()
             
     #     fname = request.POST['fname']
     #     lname = request.POST['lname']
