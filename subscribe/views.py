46edc8252032a4d30c5bec106a14bc5f42916bfa
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-08 17:04:32 +0530

    Created Thank_You.html Page and Updated Views.py file and urls.py file to add Thank You Page

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from subscribe.models import Subscribe
 from subscribe.forms import SubscribeForm
 
@@ -17,6 +18,7 @@ def subscribe(request):
             
             subscribe = Subscribe(first_name= fname, last_name= lname, email= email)
             subscribe.save()
+            return redirect(reverse('thank_you'))
             
     #     fname = request.POST['fname']
     #     lname = request.POST['lname']
@@ -30,3 +32,7 @@ def subscribe(request):
 
     context = {"form":subscribe_form, "email_error": email_error}
     return render(request, "subscribe/subscribe.html", context)
+
+def thank_you(request):
+    context = {}
+    return render(request, 'subscribe/thank_you.html', context)
