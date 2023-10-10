d7d601cf499e9f567b1be479d2b3ad5900dc3437
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 14:34:13 +0530

    Using clean.<field_name> type of field validators to validate the data inside the fields

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -5,3 +5,9 @@ class SubscribeForm(forms.Form):
     first_name = forms.CharField(max_length=100, label="First Name", help_text="Enter First Name")
     last_name = forms.CharField(max_length=100, required=False)
     email = forms.EmailField(max_length=100, disabled=True)
+    
+    def clean_first_name(self):
+        data = self.cleaned_data['first_name']
+        if "," in data:
+            raise forms.ValidationError("Invalid First Name")
+        return data
