8e0a25283a7bab415cdf6ac4a72f087bce5aa3ce
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 14:22:13 +0530

    Using disabled argument from form options to disable the editing of particular field

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class SubscribeForm(forms.Form):
     first_name = forms.CharField(max_length=100, label="First Name", help_text="Enter First Name")
     last_name = forms.CharField(max_length=100, required=False)
-    email = forms.EmailField(max_length=100)
+    email = forms.EmailField(max_length=100, disabled=True)
