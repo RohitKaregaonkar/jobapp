334d576d8fe4f7cde669e46d757165c9db8353e5
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 18:05:06 +0530

    Added option attribute inside Subscribe model and created choice for option  to select one from group and also set its default value to 'W': Weekly

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+NEWSLETTER_OPTIONS = [
+    ('W', 'Weekly'),
+    ('M', 'Monthly')
+]
+
 # Create your models here.
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     email = models.EmailField(max_length=120)
+    option = models.CharField(max_length=2, choices=NEWSLETTER_OPTIONS, default='W')
+        
