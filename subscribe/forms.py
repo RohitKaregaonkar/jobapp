330dfd24ad1ad6be5c0208225bfca5a21095cde4
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 16:02:49 +0530

    Using ModelForms to create forms by converting models into forms

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,17 +1,26 @@
 from django import forms
+from subscribe.models import Subscribe
 
-def validate_comma(value):
-    if "," in value:
-        raise forms.ValidationError("Invalid Name")
-    return value
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model =  Subscribe
+        # fields = ['first_name', 'last_name', 'email']
+        fields = '__all__'
+        
 
-class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100, label="First Name", help_text="Enter First Name")
-    last_name = forms.CharField(max_length=100, required=False, validators=[validate_comma])
-    email = forms.EmailField(max_length=100, disabled=True, validators=[validate_comma])
+
+# def validate_comma(value):
+#     if "," in value:
+#         raise forms.ValidationError("Invalid Name")
+#     return value
+
+# class SubscribeForm(forms.Form):
+#     first_name = forms.CharField(max_length=100, label="First Name", help_text="Enter First Name")
+#     last_name = forms.CharField(max_length=100, required=False, validators=[validate_comma])
+#     email = forms.EmailField(max_length=100, disabled=True, validators=[validate_comma])
     
-    def clean_first_name(self):
-        data = self.cleaned_data['first_name']
-        if "," in data:
-            raise forms.ValidationError("Invalid First Name")
-        return data
+#     def clean_first_name(self):
+#         data = self.cleaned_data['first_name']
+#         if "," in data:
+#             raise forms.ValidationError("Invalid First Name")
+#         return data
