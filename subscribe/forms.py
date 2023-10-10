ee60e7dacab6b405d5e9dca1d9daf367f782e10c
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 14:20:26 +0530

    Using help_text argument from form options to add some additional information for paticular field

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100, label="First Name")
-    last_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, label="First Name", help_text="Enter First Name")
+    last_name = forms.CharField(max_length=100, required=False)
     email = forms.EmailField(max_length=100)
