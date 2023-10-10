360dd6720c2c03adb53c3b0e977b2702d56b0673
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 17:53:28 +0530

    Changed CharField to EmailField for email attribute and registered the subscribe app in admin panel

--- subscribe/models.py
+++ subscribe/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
-    email = models.CharField(max_length=120)
+    email = models.EmailField(max_length=120)
