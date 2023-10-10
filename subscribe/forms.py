ceec876922db39a814ceb21b91e356ee9e2d866a
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 14:05:56 +0530

    Using label option from different options in forms to give labels to the fields

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100, required=False)
+    first_name = forms.CharField(max_length=100, label="First Name")
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField(max_length=100)
