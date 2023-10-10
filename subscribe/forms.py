377367bb6a063334869c29ba72a0e5ac1059dbc0
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-10 14:01:19 +0530

    Using required option from different options in forms

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, required=False)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField(max_length=100)
