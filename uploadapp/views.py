ffd7ae3a1403ad122f34585e492a7ec768f4e059
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-11 17:59:43 +0530

    Created view to upload different type of files as well as save the different types of file in database

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from uploadapp.forms import UploadForm
+from uploadapp.forms import UploadForm, UploadFileForm
 
 # Create your views here.
 def upload_image(request):
@@ -13,3 +13,14 @@ def upload_image(request):
         form = UploadForm()
     return render(request, 'uploadapp/add_image.html', {'form': form})
 
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid:
+            form.save()
+            saved_object = form.instance
+            return render(request, 'uploadapp/add_image.html', {'form': form,'saved_object': saved_object})
+    else:
+        form = UploadFileForm()
+    return render(request, 'uploadapp/add_image.html', {'form': form})
